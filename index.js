dc53fee200e9a7cc37c4cdd55690bce5d671f15a
Courtney Koko <courtney3koko@yahoo.com>
2023-08-04 14:45:19 -0500

    Save to disk then upload to google drive

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const md5 = require("md5");
+const path = require('path');
+const mime = require('mime-types');
+
 const cors = require('cors');
 const Multer = require('multer');
 const { Readable } = require('stream');
@@ -11,7 +16,7 @@ const busboy = require('busboy');
 require('dotenv').config();
 
 //Cros Setup to limit domainds
-const allowedOrigins = ["https://example.co", "http://localhost:3000"];
+const allowedOrigins = ["http://statewidenetwork.com", "http://localhost:3000"];
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -90,8 +95,19 @@ const addRowToSheet = async (sheetName, headerData, row) => {
 
 
 const app = express();
+app.use(bodyParser.raw({ type: 'application/octet-stream', limit: '100mb' }));
 app.use(cors(corsOptions))
+
+
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Replace '*' with the specific allowed origin if needed
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.get("/", (request, response) => {
   console.log(process.env.SENDGRID_API_KEY);
@@ -168,23 +184,92 @@ const setFilePublicAccess = async (fileId) => {
   }
 };
 
-// Route to handle the file upload
+
+app.post('/upload', async (req, res) => {
+  const { name, currentChunkIndex, totalChunks } = req.query;
+  const firstChunk = parseInt(currentChunkIndex) === 0;
+  const lastChunk = parseInt(currentChunkIndex) === parseInt(totalChunks) - 1;
+  const ext = name.split('.').pop();
+  const data = req.body.toString().split(',')[1];
+  const buffer = Buffer.from(data, 'base64');
+  const tmpFilename = 'tmp_' + md5(name + req.ip) + '.' + ext;
+  if (firstChunk && fs.existsSync('./uploads/' + tmpFilename)) {
+    fs.unlinkSync('./uploads/' + tmpFilename);
+  }
+  fs.appendFileSync('./uploads/' + tmpFilename, buffer);
+  if (lastChunk) {
+    console.log("last chunk")
+    const finalFilename = md5(Date.now()).substr(0, 6) + '.' + ext;
+    fs.renameSync('./uploads/' + tmpFilename, './uploads/' + finalFilename);
+    const response = await uploadToDrive(finalFilename);
+    res.status(200).json(response);
+
+    await unlinkAsync('./uploads/' + finalFilename);
+  } else {
+    res.json('ok');
+  }
+});
+
+// Upload file from "uploads" folder to Google Drive
+async function uploadToDrive(filename) {
+  console.log("upload to drive");
+  const fileMetadata = {
+    name: filename,
+    parents: [process.env.GOOGLE_DRIVE_FOLDER_ID],
+  };
+  const media = {
+    mimeType: mime.lookup('./uploads/' + filename) || 'application/octet-stream',
+    body: fs.createReadStream('./uploads/' + filename),
+  };
+
+  try {
+    console.log("try");
+    const response = await drive.files.create({
+      resource: fileMetadata,
+      media: media,
+      fields: 'id, webViewLink', // Return both the file ID and the web view link
+    });
+
+    console.log("response");
+    console.log(response);
+
+    const fileInfo = response.data;
+    const fileId = fileInfo.id;
+    await setFilePublicAccess(fileId);
+
+    return { message: 'File uploaded successfully', fileLink: fileInfo.webViewLink };
+  } catch (error) {
+    console.log("Error uploading to drive");
+    console.log(error);
+
+    return { message: 'Error uploading file', error: error };
+  }
+}
+
 
 app.post('/file', async (req, res) => {
   try {
-    const bb = busboy({ headers: req.headers });
+    const bb = busboy({
+      headers: req.headers,
+      limits: {
+        fileSize: 1024 * 1024 * 1024, // 10MB limit
+      },
+    });
 
     let fileStream;
     let fileName;
     let mimeType;
 
+    console.log("We're in the upload file route")
     bb.on('file', async (fieldname, file, filename, encoding, mimetype) => {
+      console.log("on BB")
       fileStream = file;
       fileName = filename;
       mimeType = mimetype;
 
       // Function to upload the file to Google Drive
       const uploadFileToDrive = async () => {
+        console.log("on upload to drive")
         const fileMetadata = {
           name: fileName,
           parents: [process.env.GOOGLE_DRIVE_FOLDER_ID],
