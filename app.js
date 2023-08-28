86c50b68d87822df3f2f88dd27fc8596510c27bb
Courtney Koko <courtney3koko@yahoo.com>
2023-08-28 09:59:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Im looking 4 you")
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('This is my first response');
+});
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
