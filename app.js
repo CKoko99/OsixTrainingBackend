d7c5317c4d930c613f33f65707b34653c565fc79
Courtney Koko <courtney3koko@yahoo.com>
2023-08-28 12:02:44 -0500

    Trying env variables

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('This is my updated response');
+    res.end(process.env.test || 'Hello World');
 });
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
