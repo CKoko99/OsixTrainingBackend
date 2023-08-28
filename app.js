fd818d869c55da842bea74fb8fa98cd147f515bb
Courtney Koko <courtney3koko@yahoo.com>
2023-08-28 12:17:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
-    res.end(process.env.TEST || 'Hello World');
+    res.end(JSON.parse(process.env.TEST) || 'Hello World');
 });
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
