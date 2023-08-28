b1ff6b5bf5e67dec00b953a351954a025109d345
Courtney Koko <courtney3koko@yahoo.com>
2023-08-28 11:40:47 -0500

    Add deployment to cloudbuild

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('This is my first response');
+    res.end('This is my updated response');
 });
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
