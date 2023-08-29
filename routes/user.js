4e0208de7aa394f885650f41a07724df2857eed1
Courtney Koko <courtney3koko@yahoo.com>
2023-08-29 14:14:50 -0500

    Move GetUserData to backend

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,30 @@ import { db } from "../firebase.js";
 import { doc, setDoc, getDoc, arrayUnion, collection, query, where, getDocs } from "firebase/firestore";
 
 const router = Router(); // Create an instance of the Router
+router.get("/:userId", async (req, res) => {
+    const userId = req.params.userId;
+    try {
+        const userRef = doc(db, "Users", userId);
+        // Now you can fetch the data for the specific user document
+        const docSnapshot = await getDoc(userRef);
+        if (docSnapshot.exists()) {
+            const userData = docSnapshot.data();;
+            //console.log(userData)
+            res.json({ ...userData });
+        } else {
+            //console.log("User does not exist in firestore")
+            await setDoc(userRef, {
+                displayName: user.displayName,
+                // Add any other user data fields here if needed
+            });
+            res.json({ userData: { displayName: user.displayName } });
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: error.message });
+    }
+}
+)
 
 router.post("/form", async (req, res) => {
     console.log(req.body)
