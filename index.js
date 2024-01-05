bb285ac9a2dbc66db39d6fa1f3788f96da79d70d
Courtney Koko <courtney3koko@yahoo.com>
2024-01-05 12:32:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -320,5 +320,6 @@ app.use('/store', storeRoute)
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   //console log the url to the server
+  console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
   console.log(`Server is running! on port: ${PORT}`)
 });
